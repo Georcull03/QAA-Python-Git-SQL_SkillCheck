ab30b525b1878d29e6c2052a13646d9447a6f687
Georcull03 <george@pkhome.co.uk>
2023-01-24 16:26:15 +0000

    Added test function for read_by_id in service.py

--- _tests_/test_service.py
+++ _tests_/test_service.py
@@ -2,15 +2,21 @@
 # Unit tests are not testing the entire app, just this single function
 # When testing you should be using a TEST_DB, to easily do this modify the db.py so it is using a new DB
 # When testing on a test_db you should automatically reset the DB after testing and populate it with some data before testing
+from service import *
+
 
 def test_read_by_id():
-    # Arrange - Variables and values needed for the test
-    test_id = 1
+#     # Arrange - Variables and values needed for the test
+    test_id = 6
     result = None
-    expected = Order("test name", "test mocha", False, 1.23)
+    expected = [(6, 'John', 'Small', 'Toffee', 5.75)]
 
-    # Act - What you are testing 
-    result = controller.read_by_id(test_id)
+#     # Act - What you are testing 
+    result = read_by_id(test_id)
 
-    # Assert - Asserting whether what you assume happens, does happen
+#     # Assert - Asserting whether what you assume happens, does happen
     assert result == expected
+
+# def test_get_all():
+#     result = None
+#     expected = get_all()
