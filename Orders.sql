1ad5891a93db3c0077abf86e1942edf23d686cf9
Georcull03 <george@pkhome.co.uk>
2023-01-24 11:07:09 +0000

    Wrote all functions for db.py to interact with db

--- Orders.sql
+++ Orders.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS orders (
     customer_name VARCHAR(20) NOT NULL,
     drink_size VARCHAR(20) NOT NULL,
     extras VARCHAR(35),
-    price INT NOT NULL
-)
+    price FLOAT NOT NULL
+);
 
 INSERT INTO orders (customer_name, drink_size, price) VALUES ('Jerry', 'Medium', 6.50);
-INSERT INTO orders (fish_eaten, penguin_name) VALUES ('Rebecca', 'Large', 'Whipped cream, Marshmallows', 9.00);
+INSERT INTO orders (customer_name, drink_size, extras, price) VALUES ('Rebecca', 'Large', 'Whipped cream, Marshmallows', 9.10);
--- db.py
+++ db.py
@@ -1,8 +1,39 @@
 # The DB file contains the processes and functions to create our Database or connect to it as well as run the initial SQL
 # The DB file should also contain query functions that the Service file can use to read or modify the data
 
-import sqlite3
+import sqlite3 as sql
 
-def setupConn(db_name):
-    conn = sqlite3.connect(db_name)
-    return conn
+conn = sql.connect('QACafe-db')
+
+cursor = conn.cursor()
+
+def setup_table():
+    sql_file = open('Orders.sql')
+    sql_string = sql_file.read()
+    cursor.executescript(sql_string)
+
+def run_query(query):
+    data = cursor.execute(query).fetchall()
+    return data
+
+def view_all_records():
+    query = 'SELECT * FROM orders;'
+    data = run_query(query)
+    return data
+
+def create_order(customer_name, drink_size, extras, price):
+    query = f"INSERT INTO orders (customer_name, drink_size, extras, price) VALUES ('{customer_name}', '{drink_size}', '{extras}', {price});"
+    run_query(query)
+    return True
+
+def commit_changes():
+    conn.commit()
+
+
+# setup_table()
+
+# create_order("George", "Small", "Marshmallows, Vanilla", 4.10)
+print(view_all_records())
+
+conn.commit()
+
