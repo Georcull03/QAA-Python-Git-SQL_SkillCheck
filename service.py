66a7bdde99a016903e94802a2996794fb3887eec
Georcull03 <george@pkhome.co.uk>
2023-01-24 11:35:29 +0000

    Added functions to service.py for each process

--- service.py
+++ service.py
@@ -1,7 +1,23 @@
 # The service file interacts with the DB file to Query or Modify data within the database
 # Typically there will be a function for each process that is required, and these will take in data and return data
 
-# Not a complete function, but a suggestion of what to do
+import db
+
+def get_all():
+    data = db.view_all_records()
+    return data
+
 def read_by_id(id):
-    order_data = db.query(id)
-    order = order(order_data)
+    query = f'SELECT * FROM orders WHERE order_id = {id};'
+    data = db.run_query(query)
+    return data 
+
+def create_order(customer_name, drink_size, extras, price):
+    query = f"INSERT INTO orders (customer_name, drink_size, extras, price) VALUES ('{customer_name}', '{drink_size}', '{extras}', {price});"
+    db.run_query(query)
+    return True
+
+def delete_order(id):
+    query = f'DELETE FROM orders WHERE order id = {id}'
+    db.run_query(query)
+    return True
